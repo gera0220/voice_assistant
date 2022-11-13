195376a1c275ca52e86e32576a8d2f78e7bbeb76
Erick137 <e-gm99@hotmail.com>
2022-11-12 18:05:49 -0700

    Actulización(12/11/2022)de los códigos importantes

--- src/voice_assistant.py
+++ src/voice_assistant.py
@@ -7,6 +7,8 @@ import warnings
 import pyjokes
 import ecapture as ec
 from datetime import datetime
+import librosa
+import numpy as np
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' #Se deshace de warnings por no usar CUDA
 warnings.simplefilter(action='ignore', category=FutureWarning)
@@ -56,6 +58,7 @@ if __name__ == "__main__":
             funciones.word() 
         elif "hola" in query:
             funciones.saludo(persona)
+            print(persona) 
 
 
 """ import sqlite3
@@ -69,6 +72,21 @@ if __name__ == '__main__':
     sql = "SELECT nombre FROM personas WHERE id_persona = ?"
     cur.execute(sql, [persona])
     nombre = cur.fetchall()
-    print(nombre[0][0]) """
- 
+    print(nombre[0][0])  """
 
+""" if __name__ == '__main__':  
+   audio = funciones.takeAudio()
+    with open("microphone-results.wav", "wb") as f:
+        f.write(audio.get_wav_data())
+
+X, _ = librosa.load("chunked/erick_65.wav", res_type='kaiser_fast')
+mfccs = np.mean(librosa.feature.mfcc(X, n_mfcc=40).T,axis=0)
+mfccs = mfccs.reshape(-1, mfccs.shape[0])
+mfccs = ss.transform(mfccs)
+preds = voice_detection.predict(mfccs, verbose = 0)
+pos = np.argmax(preds)
+id = funciones.asociar_id(pos)
+print(pos)
+print(id) """
+    
+ 
