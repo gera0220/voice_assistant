8dc8b8d6eb43072d0768452f533a9ef4cfada489
gera0220 <gerapm0220@gmail.com>
2022-11-19 13:42:51 -0600

    Inserción de librerías y cambios al UI: - Inserción de funciones.py como librería - Eliminación de toolbar y button_action - Creación button_inicio y button_cerrar

--- src/app.py
+++ src/app.py
@@ -9,34 +9,25 @@ from PyQt5.QtCore import Qt
 sys.path.insert(0, 'src/borrar.py')
 import borrar
 
+sys.path.insert(0, 'src/funciones.py')
+import funciones
+
 class MainWindow(QMainWindow):
 
     def __init__(self):
         super(MainWindow, self).__init__()
 
-        self.setWindowTitle("My Awesome App")
+        self.setWindowTitle("Merli")
 
-        label = QLabel("Hello!")
-        label.setAlignment(Qt.AlignCenter)
+        button_inicio = QPushButton('Iniciar', self)
+        button_inicio.clicked.connect(borrar.main)
+        button_inicio.resize(button_inicio.sizeHint())
+        button_inicio.move(50, 20)
 
-        self.setCentralWidget(label)
-
-        toolbar = QToolBar("My main toolbar")
-        self.addToolBar(toolbar)
-
-        button_action = QAction("Your button", self)
-        button_action.setStatusTip("This is your button")
-        button_action.triggered.connect(self.onMyToolBarButtonClick)
-        toolbar.addAction(button_action)
-
-        button_prueba = QPushButton('pruebame', self)
-        button_prueba.clicked.connect(borrar.main)
-        button_prueba.resize(button_prueba.sizeHint())
-        button_prueba.move(50, 50)
-
-
-    def onMyToolBarButtonClick(self, s):
-        print("click", s)
+        button_cerrar = QPushButton('Apagar', self)
+        button_cerrar.clicked.connect(funciones.cerrar)
+        button_cerrar.resize(button_cerrar.sizeHint())
+        button_cerrar.move(50, 50)
 
 
 app = QApplication(sys.argv)
