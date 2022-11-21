d73d351edfe2c62c6c2f09db2fb53aaf961eeda7
gera0220 <gerapm0220@gmail.com>
2022-11-21 10:49:05 -0600

    Se estableció un ícono de ventana

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ from PyQt5.QtWidgets import (
     QMainWindow, QApplication,
     QLabel, QToolBar, QAction, QStatusBar, QPushButton
 )
-from PyQt5.QtGui import QIcon
+from PyQt5 import QtGui
 from PyQt5.QtCore import Qt
 
 sys.path.insert(0, 'src/borrar.py')
@@ -16,6 +16,8 @@ class MainWindow(QMainWindow):
 
     def __init__(self):
         super(MainWindow, self).__init__()
+
+        self.setWindowIcon(QtGui.QIcon('microphone-logo.png'))
 
         self.setWindowTitle("Merli")
 
